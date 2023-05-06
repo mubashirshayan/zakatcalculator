1475ec954ca5051b4df074262d5ab532ba55ffd9
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-05-06 19:18:15 +0500

    New files

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log('Hello World!');
+/*var goldPri=
+var silverPri=*/
+
+/*var silver1=document.getElementById('silvr').value;
+silver1*2*/
+function bill() {
+ var silver1=document.getElementById('silvr').value;
+silver1*=2339
+var gold=document.getElementById('golds').value;
+gold*=207700
+var ruppe=document.getElementById('rupes').value;
+ruppe*=1
+
+//ruppe;
+var zatotal=(silver1+gold+ruppe)/40;
+  document.getElementById('total').innerHTML=zatotal
+}
+// document.getElementById('total').innerHTML=
+
+
+
+
